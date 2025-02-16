d3e98cb73e9915859a41405b1a81e9dcea0eb3f7
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2025-02-16 10:17:07 -0500

    player movement added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [maze, setMaze] = useState([
-    ['block', 'block', 'block', 'path'],
-    ['start', 'path', 'path', 'block'],
-    ['block', 'path', 'block', 'block'],
-    ['path', 'path', 'path', 'block'],
-    ['path', 'block', 'block', 'end'],
-    ['path', 'path', 'path', 'block']
-  ]);
+  const [maze, setMaze] = useState([]);
+  const [enemies, setEnemies] = useState([]);
+  const [playerPosition, setPlayerPosition] = useState({ x: 1, y: 0 });
+
+  useEffect(()=>{
+    handleRandomizeGrid(10, 10);
+  }, []);
+
   
   const handleRandomizeGrid = (height, width) => {
     let grid = Array.from({length:height}, () => Array(width).fill('block'));
@@ -35,20 +35,51 @@ function App() {
       }
     }
     createPath(1, 1);
-    setMaze(grid);
+    
     grid[1][0] = 'start';
     grid[height - 2][width - 1] = 'end';
+    setMaze(grid);
+    setPlayerPosition({ x: 1, y: 1 });
 
   }
 
+  const handleKeyDown = (e)=>{
+    const {x, y} = playerPosition;
+    let newX = x;
+    let newY = y;
+
+    if(e.key === 'ArrowUp'){
+      newY -= 1; 
+    }
+    if(e.key === 'ArrowDown'){
+      newY += 1; 
+    }
+    if(e.key === 'ArrowLeft'){
+      newX -= 1; 
+    }
+    if(e.key === 'ArrowRight'){
+      newX += 1; 
+    }
+    if (newX >=0 && newY >=0 && newX < maze[0].length && newY < maze.length &&maze[newY][newX]!= 'block'){
+      setPlayerPosition({x: newX, y: newY});
+    }
+  }
+
   return (
-    <div className='container'>
-      <button onClick={() => handleRandomizeGrid(6, 4)}>Randomize Grid</button>
-      <div className='maze-grid'>
+    <div className='container' onKeyDown={handleKeyDown} >
+      <button onClick={() => handleRandomizeGrid(10, 10)}>Randomize Grid</button>
+      <div className='maze-grid'  tabIndex={0}>
         {maze.map((row, rowIndex) => (
           <div key={rowIndex} className='row'>
             {row.map((cell, cellIndex) => (
-              <div key={cellIndex} className={`cell ${cell}`}></div>
+             <div
+             key={cellIndex}
+             className={`cell ${cell} ${
+               playerPosition.x === cellIndex && playerPosition.y === rowIndex
+                 ? 'player'
+                 : ''
+             }`}
+           ></div>
             ))}
           </div>
         ))}
